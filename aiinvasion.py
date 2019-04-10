494bfe8f166a65fed44dce5661ce0b52eedb644c
terbaco <terbaco@users.noreply.github.com>
2019-04-10 17:25:41 +0800

    fix quick collision issue
    Update ship position after collision

---
+++ aiinvasion.py
@@ -0,0 +1,41 @@
+import sys
+
+import pygame
+from pygame.sprite import Group
+
+from settings import Settings
+from ship import Ship
+import game_functions as gf
+from alien import Alien
+from game_stats import GameStats
+from button import Button
+from scoreboard import Scoreboard
+from showbar import ShowBar
+
+def run_game():
+    pygame.init()
+    ai_setting = Settings()
+    stats = GameStats(ai_setting)
+    screen = pygame.display.set_mode((ai_setting.screen_width, ai_setting.screen_height))
+    pygame.display.set_caption("AlienInvasion")
+
+    play_button = Button(ai_setting, screen, "Play")
+    ship = Ship(ai_setting, screen, stats)
+    bullets = Group()
+    aliens = Group()
+
+#    sb = Scoreboard(ai_setting, screen, stats)
+    show_highest = ShowBar(ai_setting, screen, stats, 'righttop', 'Hightest Record')
+    sb = ShowBar(ai_setting, screen, stats, 'rightbottom', 'Score')
+    show_level = ShowBar(ai_setting, screen, stats, 'leftbottom', 'Level')
+    while True:
+        gf.check_events(ai_setting, screen, stats, play_button, ship, aliens, bullets)
+        if stats.game_active:
+            ship.update()
+            gf.update_aliens(ai_setting, stats, screen,ship, aliens, bullets)
+            gf.update_bullets(ai_setting, screen, stats, ship, aliens, bullets)
+#            gf.create_fleet(ai_setting, screen, aliens)
+        gf.update_screen(ai_setting, screen, stats, sb, ship, aliens, bullets, play_button, show_level, show_highest)
+
+
+run_game()
---
+++ ship.py
@@ -0,0 +1,49 @@
+import pygame
+
+class Ship():
+
+    def __init__(self, ai_settings, screen, stats):
+        self.screen = screen
+        self.ai_setting = ai_settings
+        self.stats = stats
+#        self.speed = 1.0
+
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
+
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.centery + 300
+
+        self.centerx = float(self.rect.centerx)
+        self.centery = float(self.rect.centery)
+
+        self.moving_right = False
+        self.moving_left = False
+        self.moving_down = False
+        self.moving_up = False
+
+    def update(self):
+        moving = self.ai_setting.ship_speed_factor +\
+                 self.ai_setting.ship_speed_factor_step * self.stats.game_level
+        if self.moving_right and (self.rect.right + moving) < self.screen_rect.right:
+            self.centerx += moving
+        elif self.moving_left and (self.rect.left - moving) > self.screen_rect.left:
+            self.centerx -= moving
+        elif self.moving_down and (self.rect.bottom + moving) < self.screen_rect.bottom:
+            self.centery += moving
+        elif self.moving_up and (self.rect.top - moving) > self.screen_rect.top:
+            self.centery -= moving
+
+        self.rect.centerx = int(self.centerx + 0.5)
+        self.rect.centery = int(self.centery + 0.5)
+
+    def blitme(self):
+        self.screen.blit(self.image, self.rect)
+
+    def center_ship(self):
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.centery + 300
+
+        self.centerx = float(self.rect.centerx)
+        self.centery = float(self.rect.centery)
